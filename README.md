5d5f3b3a02494faea961303aa20780d58afe9a49
kerokerotur <kerokerotur2000@gmail.com>
2025-03-19 01:02:51 +0900

    feat: 画像生成手順作成 fix: 画像生成の予約語にdefaultが使われているらしいのでnormalに変更

--- README.md
+++ README.md
@@ -11,22 +11,31 @@
 - この辺参考にして設定・使うと良さそう
   - https://zenn.dev/altiveinc/articles/flutter-version-management
 
+# 開発Tips
+
 ## 環境変数の設定
 - envファイルを修正するだけでOK
 - 根本的な設定は以下を参考に実装している
   - https://zenn.dev/altiveinc/articles/separating-environments-in-flutter
 
-### スプラッシュ画面の設定
+## スプラッシュ画面の設定
 - pubspec.yamlに設定を記載している
 - `derry gen-splash`を実行する
 - この辺を参考にして設定
   - https://zenn.dev/susatthi/articles/20220406-061305-flutter-native-splash
 
-### アプリアイコンの設定
+## アプリアイコンの設定
 - flutter_launcher_icons-{FLAVOR}.yamlファイルを作成し、設定したいアイコン画像へのパスを作成する
 - `derry gen-icon`を実行する
 
-### アプリ名の設定
+## アプリ名の設定
 - .envファイルを修正する
 - envファイルの内容をどうやって各プラットフォームに反映しているかは下記記事を参考
   - https://zenn.dev/altiveinc/articles/separating-environments-in-flutter
+
+## 画像追加手順
+- この辺を参考にして設定
+  - https://flutter.salon/plugin/fluttergen/
+- pubspec.yamlにフォルダを追記しないと自動生成対象にならないことに注意
+- `derry gen`を実行する
+- asset.gen.dartに自動生成されるので、画像ファイル名を型安全に使用できる
---
+++ client/lib/gen/assets.gen.dart
@@ -0,0 +1,455 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// Directory path: assets/images/background
+  $AssetsImagesBackgroundGen get background =>
+      const $AssetsImagesBackgroundGen();
+
+  /// Directory path: assets/images/playingcard
+  $AssetsImagesPlayingcardGen get playingcard =>
+      const $AssetsImagesPlayingcardGen();
+}
+
+class $AssetsImagesBackgroundGen {
+  const $AssetsImagesBackgroundGen();
+
+  /// File path: assets/images/background/game.png
+  AssetGenImage get game =>
+      const AssetGenImage('assets/images/background/game.png');
+
+  /// File path: assets/images/background/top.png
+  AssetGenImage get top =>
+      const AssetGenImage('assets/images/background/top.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [game, top];
+}
+
+class $AssetsImagesPlayingcardGen {
+  const $AssetsImagesPlayingcardGen();
+
+  /// Directory path: assets/images/playingcard/normal
+  $AssetsImagesPlayingcardNormalGen get normal =>
+      const $AssetsImagesPlayingcardNormalGen();
+}
+
+class $AssetsImagesPlayingcardNormalGen {
+  const $AssetsImagesPlayingcardNormalGen();
+
+  /// File path: assets/images/playingcard/normal/back.png
+  AssetGenImage get back =>
+      const AssetGenImage('assets/images/playingcard/normal/back.png');
+
+  /// Directory path: assets/images/playingcard/normal/club
+  $AssetsImagesPlayingcardNormalClubGen get club =>
+      const $AssetsImagesPlayingcardNormalClubGen();
+
+  /// Directory path: assets/images/playingcard/normal/dia
+  $AssetsImagesPlayingcardNormalDiaGen get dia =>
+      const $AssetsImagesPlayingcardNormalDiaGen();
+
+  /// Directory path: assets/images/playingcard/normal/heart
+  $AssetsImagesPlayingcardNormalHeartGen get heart =>
+      const $AssetsImagesPlayingcardNormalHeartGen();
+
+  /// Directory path: assets/images/playingcard/normal/joker
+  $AssetsImagesPlayingcardNormalJokerGen get joker =>
+      const $AssetsImagesPlayingcardNormalJokerGen();
+
+  /// Directory path: assets/images/playingcard/normal/spade
+  $AssetsImagesPlayingcardNormalSpadeGen get spade =>
+      const $AssetsImagesPlayingcardNormalSpadeGen();
+
+  /// List of all assets
+  List<AssetGenImage> get values => [back];
+}
+
+class $AssetsImagesPlayingcardNormalClubGen {
+  const $AssetsImagesPlayingcardNormalClubGen();
+
+  /// File path: assets/images/playingcard/normal/club/1.png
+  AssetGenImage get a1 =>
+      const AssetGenImage('assets/images/playingcard/normal/club/1.png');
+
+  /// File path: assets/images/playingcard/normal/club/10.png
+  AssetGenImage get a10 =>
+      const AssetGenImage('assets/images/playingcard/normal/club/10.png');
+
+  /// File path: assets/images/playingcard/normal/club/11.png
+  AssetGenImage get a11 =>
+      const AssetGenImage('assets/images/playingcard/normal/club/11.png');
+
+  /// File path: assets/images/playingcard/normal/club/12.png
+  AssetGenImage get a12 =>
+      const AssetGenImage('assets/images/playingcard/normal/club/12.png');
+
+  /// File path: assets/images/playingcard/normal/club/13.png
+  AssetGenImage get a13 =>
+      const AssetGenImage('assets/images/playingcard/normal/club/13.png');
+
+  /// File path: assets/images/playingcard/normal/club/2.png
+  AssetGenImage get a2 =>
+      const AssetGenImage('assets/images/playingcard/normal/club/2.png');
+
+  /// File path: assets/images/playingcard/normal/club/3.png
+  AssetGenImage get a3 =>
+      const AssetGenImage('assets/images/playingcard/normal/club/3.png');
+
+  /// File path: assets/images/playingcard/normal/club/4.png
+  AssetGenImage get a4 =>
+      const AssetGenImage('assets/images/playingcard/normal/club/4.png');
+
+  /// File path: assets/images/playingcard/normal/club/5.png
+  AssetGenImage get a5 =>
+      const AssetGenImage('assets/images/playingcard/normal/club/5.png');
+
+  /// File path: assets/images/playingcard/normal/club/6.png
+  AssetGenImage get a6 =>
+      const AssetGenImage('assets/images/playingcard/normal/club/6.png');
+
+  /// File path: assets/images/playingcard/normal/club/7.png
+  AssetGenImage get a7 =>
+      const AssetGenImage('assets/images/playingcard/normal/club/7.png');
+
+  /// File path: assets/images/playingcard/normal/club/8.png
+  AssetGenImage get a8 =>
+      const AssetGenImage('assets/images/playingcard/normal/club/8.png');
+
+  /// File path: assets/images/playingcard/normal/club/9.png
+  AssetGenImage get a9 =>
+      const AssetGenImage('assets/images/playingcard/normal/club/9.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+    a1,
+    a10,
+    a11,
+    a12,
+    a13,
+    a2,
+    a3,
+    a4,
+    a5,
+    a6,
+    a7,
+    a8,
+    a9,
+  ];
+}
+
+class $AssetsImagesPlayingcardNormalDiaGen {
+  const $AssetsImagesPlayingcardNormalDiaGen();
+
+  /// File path: assets/images/playingcard/normal/dia/1.png
+  AssetGenImage get a1 =>
+      const AssetGenImage('assets/images/playingcard/normal/dia/1.png');
+
+  /// File path: assets/images/playingcard/normal/dia/10.png
+  AssetGenImage get a10 =>
+      const AssetGenImage('assets/images/playingcard/normal/dia/10.png');
+
+  /// File path: assets/images/playingcard/normal/dia/11.png
+  AssetGenImage get a11 =>
+      const AssetGenImage('assets/images/playingcard/normal/dia/11.png');
+
+  /// File path: assets/images/playingcard/normal/dia/12.png
+  AssetGenImage get a12 =>
+      const AssetGenImage('assets/images/playingcard/normal/dia/12.png');
+
+  /// File path: assets/images/playingcard/normal/dia/13.png
+  AssetGenImage get a13 =>
+      const AssetGenImage('assets/images/playingcard/normal/dia/13.png');
+
+  /// File path: assets/images/playingcard/normal/dia/2.png
+  AssetGenImage get a2 =>
+      const AssetGenImage('assets/images/playingcard/normal/dia/2.png');
+
+  /// File path: assets/images/playingcard/normal/dia/3.png
+  AssetGenImage get a3 =>
+      const AssetGenImage('assets/images/playingcard/normal/dia/3.png');
+
+  /// File path: assets/images/playingcard/normal/dia/4.png
+  AssetGenImage get a4 =>
+      const AssetGenImage('assets/images/playingcard/normal/dia/4.png');
+
+  /// File path: assets/images/playingcard/normal/dia/5.png
+  AssetGenImage get a5 =>
+      const AssetGenImage('assets/images/playingcard/normal/dia/5.png');
+
+  /// File path: assets/images/playingcard/normal/dia/6.png
+  AssetGenImage get a6 =>
+      const AssetGenImage('assets/images/playingcard/normal/dia/6.png');
+
+  /// File path: assets/images/playingcard/normal/dia/7.png
+  AssetGenImage get a7 =>
+      const AssetGenImage('assets/images/playingcard/normal/dia/7.png');
+
+  /// File path: assets/images/playingcard/normal/dia/8.png
+  AssetGenImage get a8 =>
+      const AssetGenImage('assets/images/playingcard/normal/dia/8.png');
+
+  /// File path: assets/images/playingcard/normal/dia/9.png
+  AssetGenImage get a9 =>
+      const AssetGenImage('assets/images/playingcard/normal/dia/9.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+    a1,
+    a10,
+    a11,
+    a12,
+    a13,
+    a2,
+    a3,
+    a4,
+    a5,
+    a6,
+    a7,
+    a8,
+    a9,
+  ];
+}
+
+class $AssetsImagesPlayingcardNormalHeartGen {
+  const $AssetsImagesPlayingcardNormalHeartGen();
+
+  /// File path: assets/images/playingcard/normal/heart/1.png
+  AssetGenImage get a1 =>
+      const AssetGenImage('assets/images/playingcard/normal/heart/1.png');
+
+  /// File path: assets/images/playingcard/normal/heart/10.png
+  AssetGenImage get a10 =>
+      const AssetGenImage('assets/images/playingcard/normal/heart/10.png');
+
+  /// File path: assets/images/playingcard/normal/heart/11.png
+  AssetGenImage get a11 =>
+      const AssetGenImage('assets/images/playingcard/normal/heart/11.png');
+
+  /// File path: assets/images/playingcard/normal/heart/12.png
+  AssetGenImage get a12 =>
+      const AssetGenImage('assets/images/playingcard/normal/heart/12.png');
+
+  /// File path: assets/images/playingcard/normal/heart/13.png
+  AssetGenImage get a13 =>
+      const AssetGenImage('assets/images/playingcard/normal/heart/13.png');
+
+  /// File path: assets/images/playingcard/normal/heart/2.png
+  AssetGenImage get a2 =>
+      const AssetGenImage('assets/images/playingcard/normal/heart/2.png');
+
+  /// File path: assets/images/playingcard/normal/heart/3.png
+  AssetGenImage get a3 =>
+      const AssetGenImage('assets/images/playingcard/normal/heart/3.png');
+
+  /// File path: assets/images/playingcard/normal/heart/4.png
+  AssetGenImage get a4 =>
+      const AssetGenImage('assets/images/playingcard/normal/heart/4.png');
+
+  /// File path: assets/images/playingcard/normal/heart/5.png
+  AssetGenImage get a5 =>
+      const AssetGenImage('assets/images/playingcard/normal/heart/5.png');
+
+  /// File path: assets/images/playingcard/normal/heart/6.png
+  AssetGenImage get a6 =>
+      const AssetGenImage('assets/images/playingcard/normal/heart/6.png');
+
+  /// File path: assets/images/playingcard/normal/heart/7.png
+  AssetGenImage get a7 =>
+      const AssetGenImage('assets/images/playingcard/normal/heart/7.png');
+
+  /// File path: assets/images/playingcard/normal/heart/8.png
+  AssetGenImage get a8 =>
+      const AssetGenImage('assets/images/playingcard/normal/heart/8.png');
+
+  /// File path: assets/images/playingcard/normal/heart/9.png
+  AssetGenImage get a9 =>
+      const AssetGenImage('assets/images/playingcard/normal/heart/9.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+    a1,
+    a10,
+    a11,
+    a12,
+    a13,
+    a2,
+    a3,
+    a4,
+    a5,
+    a6,
+    a7,
+    a8,
+    a9,
+  ];
+}
+
+class $AssetsImagesPlayingcardNormalJokerGen {
+  const $AssetsImagesPlayingcardNormalJokerGen();
+
+  /// File path: assets/images/playingcard/normal/joker/joker.png
+  AssetGenImage get joker =>
+      const AssetGenImage('assets/images/playingcard/normal/joker/joker.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [joker];
+}
+
+class $AssetsImagesPlayingcardNormalSpadeGen {
+  const $AssetsImagesPlayingcardNormalSpadeGen();
+
+  /// File path: assets/images/playingcard/normal/spade/1.png
+  AssetGenImage get a1 =>
+      const AssetGenImage('assets/images/playingcard/normal/spade/1.png');
+
+  /// File path: assets/images/playingcard/normal/spade/10.png
+  AssetGenImage get a10 =>
+      const AssetGenImage('assets/images/playingcard/normal/spade/10.png');
+
+  /// File path: assets/images/playingcard/normal/spade/11.png
+  AssetGenImage get a11 =>
+      const AssetGenImage('assets/images/playingcard/normal/spade/11.png');
+
+  /// File path: assets/images/playingcard/normal/spade/12.png
+  AssetGenImage get a12 =>
+      const AssetGenImage('assets/images/playingcard/normal/spade/12.png');
+
+  /// File path: assets/images/playingcard/normal/spade/13.png
+  AssetGenImage get a13 =>
+      const AssetGenImage('assets/images/playingcard/normal/spade/13.png');
+
+  /// File path: assets/images/playingcard/normal/spade/2.png
+  AssetGenImage get a2 =>
+      const AssetGenImage('assets/images/playingcard/normal/spade/2.png');
+
+  /// File path: assets/images/playingcard/normal/spade/3.png
+  AssetGenImage get a3 =>
+      const AssetGenImage('assets/images/playingcard/normal/spade/3.png');
+
+  /// File path: assets/images/playingcard/normal/spade/4.png
+  AssetGenImage get a4 =>
+      const AssetGenImage('assets/images/playingcard/normal/spade/4.png');
+
+  /// File path: assets/images/playingcard/normal/spade/5.png
+  AssetGenImage get a5 =>
+      const AssetGenImage('assets/images/playingcard/normal/spade/5.png');
+
+  /// File path: assets/images/playingcard/normal/spade/6.png
+  AssetGenImage get a6 =>
+      const AssetGenImage('assets/images/playingcard/normal/spade/6.png');
+
+  /// File path: assets/images/playingcard/normal/spade/7.png
+  AssetGenImage get a7 =>
+      const AssetGenImage('assets/images/playingcard/normal/spade/7.png');
+
+  /// File path: assets/images/playingcard/normal/spade/8.png
+  AssetGenImage get a8 =>
+      const AssetGenImage('assets/images/playingcard/normal/spade/8.png');
+
+  /// File path: assets/images/playingcard/normal/spade/9.png
+  AssetGenImage get a9 =>
+      const AssetGenImage('assets/images/playingcard/normal/spade/9.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+    a1,
+    a10,
+    a11,
+    a12,
+    a13,
+    a2,
+    a3,
+    a4,
+    a5,
+    a6,
+    a7,
+    a8,
+    a9,
+  ];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName, {this.size, this.flavors = const {}});
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.medium,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({AssetBundle? bundle, String? package}) {
+    return AssetImage(_assetName, bundle: bundle, package: package);
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
